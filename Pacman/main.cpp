d1a79a07d0339f3c67e8d7faad3a1ec1c2525653
ECS\meadowsje <jeremybmeadows@gmail.com>
2017-10-26 13:35:04 -0500

    Changed Main back

--- Pacman/main.cpp
+++ Pacman/main.cpp
@@ -2,32 +2,41 @@
 #include "line.h"
 #include "point.h"
 #include "SDL_Plotter.h"
+#include "functions.h"
 
 using namespace std;
 
-
-int main(int argc, char** argv)
+int main(int argc, char ** argv)
 {
-    SDL_Plotter g(1000, 1000);
-    Line lineOne(Point(20, 20), Point(21, 900));
+    SDL_Plotter g(1000,1000);
+    int x,y, xd, yd;
+    int R, G, B;
+    int dir = 0;
+    char key;
+    x = y = 500;
 
-    while(!g.getQuit()){
-        if(g.kbhit()){
+    while (!g.getQuit())
+    {
+        drawGhost(g, 250, 250, 210, 30, 10);
+        drawGhost(g, 250, 750, 0, 0, 220);
+        drawGhost(g, 750, 250, 180, 60, 100);
+        drawGhost(g, 750, 750, 140, 100, 0);
+    	drawPacman(g, x, y);
+    	g.update();
+    	g.Sleep(10);
+    	clearPacman(g, x, y);
+    	movePacman(x, y, dir);
 
-            switch(g.getKey()){
-                case 'P': lineOne.draw(cout);
-                          lineOne.draw(g);
-                          g.update();
-                          break;
-                case 'R': lineOne.setColor(Color(200, 15, 15));
-                          lineOne.draw(g);
-                          g.update();
-                          break;
-
-            }
-        }
+    	if(g.kbhit())
+        {
+    	    key = g.getKey();
+    	    switch(key)
+    	    {
+    	        case RIGHT_ARROW: dir = RIGHT; break;
+    	        case LEFT_ARROW: dir = LEFT; break;
+    	        case UP_ARROW: dir = UP; break;
+    	        case DOWN_ARROW: dir = DOWN; break;
+    	    }
+    	}
     }
-
-
-    return 0;
 }
